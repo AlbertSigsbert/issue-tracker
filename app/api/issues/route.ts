aacd5006d877b633319fbbae518a83d438ee8910
ALBERT <ishengomaalbert@gmail.com>
2023-11-21 15:01:51 +0300

    Setup Shadcn Library

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -5,7 +5,7 @@ import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
   title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  description: z.string().min(1)
 });
 
 export async function POST(request: NextRequest) {
@@ -13,10 +13,10 @@ export async function POST(request: NextRequest) {
 
   const validation = createIssueSchema.safeParse(body);
 
-  if (!validation.success) {
+  if (!validation.success) 
     return NextResponse.json(validation.error.errors, { status: 400 });
-  }
-
+  
+    
   const newIsssue = await prisma.issue.create({
     data: { title: body.title, description: body.description },
   });
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Issues() {
   return (
-    <div>Issues</div>
+    <Button>New Issue</Button>
   )
 }
